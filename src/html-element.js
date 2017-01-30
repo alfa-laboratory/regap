ecd76fe94d713fb32549e947ccf066e1cef3a509
Vitaliy Green <point.green@gmail.com>
2017-01-30 22:05:51 +0300

    feat(*): add callbacks

--- src/html-element.js
+++ src/html-element.js
@@ -6,6 +6,17 @@ import SlotsMixin from './slots-mixin';
 import CallbacksMixin from './callbacks-mixin';
 import PublicMethodsMixin from './public-methods-mixin';
 
+const CALLBACKS = [
+    'onBeforeCreated',
+    'onAfterCreated',
+    'onBeforeAttached',
+    'onAfterAttached',
+    'onBeforeAttributeChanged',
+    'onAfterAttributeChanged',
+    'onBeforeDetached',
+    'onAfterDetached'
+];
+
 const Base = {
     _options: {},
     _reactComponentCtor: null,
@@ -16,20 +27,64 @@ const Base = {
     },
 
     createdCallback() {
+        this._applyCallbacks();
+
+        if (this._onBeforeCreated) {
+            this._onBeforeCreated();
+        }
+
         this.regap = true;
         this._refProp = this._refProp.bind(this);
         this._initializeAttrs(this._options.attrs || {});
         this._initializeCallbacks(this._options.callbacks || {});
         this._initializePublicMethods(null, this._options.methods || {});
         this._initializeSlots(this._options.slots || {});
+
+        if (this._onAfterCreated) {
+            this._onAfterCreated();
+        }
     },
 
     attachedCallback() {
+        if (this._onBeforeAttached) {
+            this._onBeforeAttached();
+        }
+
         this._render();
+
+        if (this._onAfterAttached) {
+            this._onAfterAttached();
+        }
     },
 
-    attributeChangedCallback() {
+    attributeChangedCallback(attr, oldVal, newVal) {
+        if (this._onBeforeAttributeChanged) {
+            this._onBeforeAttributeChanged(attr, oldVal, newVal);
+        }
+
         this._render();
+
+        if (this._onAfterAttributeChanged) {
+            this._onAfterAttributeChanged(attr, oldVal, newVal);
+        }
+    },
+
+    detachedCallback() {
+        if (this._onBeforeDetached) {
+            this._onBeforeDetached();
+        }
+
+        if (this._onAfterDetached) {
+            this._onAfterDetached();
+        }
+    },
+
+    _applyCallbacks() {
+        CALLBACKS.forEach((callbackName) => {
+            if (this._options[callbackName]) {
+                this[`_${callbackName}`] = this._options[callbackName].bind(this);
+            }
+        });
     },
 
     /**
@@ -44,19 +99,23 @@ const Base = {
         };
     },
 
-    _renderReactComponent() {
+    /**
+     * @protected
+     * @param {Function} reactCtor
+     * @param {Object} props
+     */
+    _renderReactComponent(reactCtor, props) {
         ReactDOM.render(
-            React.createElement(this._reactComponentCtor, this._getRenderProps()),
+            React.createElement(reactCtor, props),
             this
         );
-
-        this._reactComponentRootElement = this.querySelector('[data-reactroot]');
     },
 
     _render() {
         let oldReactComponent = this._reactComponent;
 
-        this._renderReactComponent();
+        this._renderReactComponent(this._reactComponentCtor, this._getRenderProps());
+        this._reactComponentRootElement = this.querySelector('[data-reactroot]');
 
         if (this._reactComponent !== oldReactComponent) {
             this._initializePublicMethods(this._reactComponent, this._options.methods || {});
--- src/html-element.test.js
+++ src/html-element.test.js
@@ -22,4 +22,118 @@ describe('HtmlElement', () => {
 
         expect(component).to.have.html('<div data-reactroot="" class="html-element-component"></div>');
     });
+
+    it('should call `onBeforeCreated` callback after create component', () => {
+        let onBeforeCreated = sinon.spy();
+        createTestComponentCtor(
+            'x-before-create-call',
+            {
+                _reactComponentCtor: HtmlElementComponent,
+                _options: {
+                    onBeforeCreated
+                }
+            },
+            [RegapHtmlElement]
+        );
+
+        renderInBody('<x-before-create-call></x-before-create-call>');
+
+        expect(onBeforeCreated).to.have.been.calledOnce;
+    });
+
+    it('should call `onAfterCreated` callback after create component', () => {
+        let onAfterCreated = sinon.spy();
+        createTestComponentCtor(
+            'x-after-create-call',
+            {
+                _reactComponentCtor: HtmlElementComponent,
+                _options: {
+                    onAfterCreated
+                }
+            },
+            [RegapHtmlElement]
+        );
+
+        renderInBody('<x-after-create-call></x-after-create-call>');
+
+        expect(onAfterCreated).to.have.been.calledOnce;
+    });
+
+    it('should call `onBeforeCreated` before `onAfterCreated` callback after create component',
+    () => {
+        let onBeforeCreated = sinon.spy();
+        let onAfterCreated = sinon.spy();
+        createTestComponentCtor(
+            'x-create-call-seq',
+            {
+                _reactComponentCtor: HtmlElementComponent,
+                _options: {
+                    onBeforeCreated,
+                    onAfterCreated
+                }
+            },
+            [RegapHtmlElement]
+        );
+
+        renderInBody('<x-create-call-seq></x-create-call-seq>');
+
+        expect(onBeforeCreated).to.have.been.calledBefore(onAfterCreated);
+    });
+
+    it('should call `onBeforeAttached` callback after attach component', () => {
+        let onBeforeAttached = sinon.spy();
+        createTestComponentCtor(
+            'x-before-attach-call',
+            {
+                _reactComponentCtor: HtmlElementComponent,
+                _options: {
+                    onBeforeAttached
+                }
+            },
+            [RegapHtmlElement]
+        );
+
+        renderInBody('<x-before-attach-call></x-before-attach-call>');
+
+        expect(onBeforeAttached).to.have.been.calledOnce;
+    });
+
+    it('should call `onAfterAttached` callback after attach component', () => {
+        let onAfterAttached = sinon.spy();
+        createTestComponentCtor(
+            'x-after-attach-call',
+            {
+                _reactComponentCtor: HtmlElementComponent,
+                _options: {
+                    onAfterAttached
+                }
+            },
+            [RegapHtmlElement]
+        );
+
+        renderInBody('<x-after-attach-call></x-after-attach-call>');
+
+        expect(onAfterAttached).to.have.been.calledOnce;
+    });
+
+    it('should call `onBeforeAttached` before `onAfterAttached` callback after attach component',
+    () => {
+        let onBeforeAttached = sinon.spy();
+        let onAfterAttached = sinon.spy();
+        createTestComponentCtor(
+            'x-attach-call-seq',
+            {
+                _reactComponentCtor: HtmlElementComponent,
+                _options: {
+                    onBeforeAttached,
+                    onAfterAttached
+                }
+            },
+            [RegapHtmlElement]
+        );
+
+        renderInBody('<x-attach-call-seq></x-attach-call-seq>');
+
+        expect(onBeforeAttached).to.have.been.calledBefore(onAfterAttached);
+    });
 });
