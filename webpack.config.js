11553686a6b794e6b4d069f4853d8497ff813736
Vitaliy Green <point.green@gmail.com>
2017-06-13 22:16:58 +0300

    fix(*): index file path for tests

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './regap.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'regap.js'
